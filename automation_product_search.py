7561999e2bcf0fce489b0c3c0bf064603b057c36
Danilo Portela <daniloportela97@gmail.com>
2023-08-09 17:21:11 -0300

    Verificar se existe alguma oferta dentro da tabela de ofertas.

--- automation_product_search.py
+++ automation_product_search.py
@@ -11,20 +11,23 @@ table_products = pd.read_excel("data/buscas.xlsx")
 table_offers = list_offers_found(table_products)
 
 # enviar por e-mail o resultado da tabela
-name = "Haroldo"
-to = ["haroldo.santos@ruraltech.com.br"]
-subject = "Produto(s) Encontrado(s) na faixa de preço desejada"
-body = f"""
-<!DOCTYPE html>
-<html>
-<p>Faaaaala, {name}</p>
-<p>Encontramos alguns produtos em oferta dentro da faixa de preço desejada. Segue a tabela com detalhes</p>
-{table_offers.to_html(index=False)}
-<p>Qualquer dúvida estou à disposição</p>
-<p>Att., Portela</p>
-</html>
-"""
-path_file = Path(r"test\ofertas.xlsx")
-attachment = [Path.cwd() / path_file]
 
-send_email(attachment, to, subject, body)
+# verificando se existe alguma oferta dentro da tabela de ofertas
+if not table_offers.empty:
+    name = "Haroldo"
+    to = ["haroldo.santos@ruraltech.com.br"]
+    subject = "Produto(s) Encontrado(s) na faixa de preço desejada"
+    body = f"""
+    <!DOCTYPE html>
+    <html>
+    <p>Faaaaala, {name}</p>
+    <p>Encontramos alguns produtos em oferta dentro da faixa de preço desejada. Segue a tabela com detalhes</p>
+    {table_offers.to_html(index=False)}
+    <p>Qualquer dúvida estou à disposição</p>
+    <p>Att., Portela</p>
+    </html>
+    """
+    path_file = Path(r"test\ofertas.xlsx")
+    attachment = [Path.cwd() / path_file]
+
+    send_email(attachment, to, subject, body)
